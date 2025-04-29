0bfecf942af6eca7873a46209963cf33ca669a96
Icarus <104295984+Izzzzz27@users.noreply.github.com>
2025-04-30 00:54:30 +0300

    Update AddUser.cs

--- src/AddUser.cs
+++ src/AddUser.cs
@@ -1,18 +1,29 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Newtonsoft.Json;
+using UserSerializationProject.Models;
 
-public class User {
-    public int id { get; set; }
-    public string name { get; set; }
-    public string email { get; set; }
-}
-
-class AddUser {
-    static void Main() {
-        var users = JsonConvert.DeserializeObject<List<User>>(File.ReadAllText("users.json"));
-        users.Add(new User { id = 3, name = "lena", email = "lena@example.com" });
-        File.WriteAllText("users.json", JsonConvert.SerializeObject(users, Formatting.Indented));
+namespace UserSerializationProject
+{
+    class AddUser 
+    {
+        public static void Run() 
+        {
+            var users = JsonConvert.DeserializeObject<List<User>>(File.ReadAllText("users.json")) ?? new List<User>();
+            
+            // Generate new ID (max + 1)
+            int newId = 1;
+            if (users.Count > 0)
+            {
+                newId = users.Max(u => u.id) + 1;
+            }
+            
+            users.Add(new User { id = newId, name = "lena", email = "lena@example.com" });
+            File.WriteAllText("users.json", JsonConvert.SerializeObject(users, Formatting.Indented));
+            
+            Console.WriteLine($"User 'lena' added successfully with ID {newId}!");
+        }
     }
 }
