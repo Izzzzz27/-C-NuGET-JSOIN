7d59c9b7e8ecaa1409319c792b8421fc75a53ac6
Icarus <104295984+Izzzzz27@users.noreply.github.com>
2025-04-30 00:56:26 +0300

    Task 3: Deserialize JSON and output to console.

--- src/DeserializeUsers.cs
+++ src/DeserializeUsers.cs
@@ -2,18 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
+using UserSerializationProject.Models;
 
-public class User {
-    public int id { get; set; }
-    public string name { get; set; }
-    public string email { get; set; }
-}
-
-class DeserializeUsers {
-    static void Main() {
-        var users = JsonConvert.DeserializeObject<List<User>>(File.ReadAllText("users.json"));
-        foreach (var user in users) {
-            Console.WriteLine($"id: {user.id}, name: {user.name}, email: {user.email}");
+namespace UserSerializationProject
+{
+    class DeserializeUsers 
+    {
+        public static void Run() 
+        {
+            var users = JsonConvert.DeserializeObject<List<User>>(File.ReadAllText("users.json")) ?? new List<User>();
+            foreach (var user in users) 
+            {
+                Console.WriteLine($"id: {user.id}, name: {user.name ?? "N/A"}, email: {user.email ?? "N/A"}");
+            }
         }
     }
 }
