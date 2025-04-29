2d2368af26f71d71e0dbf662b48c1bdb23e42107
Icarus <104295984+Izzzzz27@users.noreply.github.com>
2025-04-30 00:59:26 +0300

    Task 5: Deserialize user types and output to console

--- src/DeserializeUserTypes.cs
+++ src/DeserializeUserTypes.cs
@@ -2,22 +2,24 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
+using UserSerializationProject.Models;
 
-public class User {
-    public int id { get; set; }
-    public string name { get; set; }
-    public string email { get; set; }
-}
+namespace UserSerializationProject
+{
+    public class UserWithType : User 
+    {
+        public string? userType { get; set; }
+    }
 
-public class UserWithType : User {
-    public string userType { get; set; }
-}
-
-class DeserializeUserTypes {
-    static void Main() {
-        var users = JsonConvert.DeserializeObject<List<UserWithType>>(File.ReadAllText("user_types.json"));
-        foreach (var user in users) {
-            Console.WriteLine($"{user.name} has the role of {user.userType}");
+    class DeserializeUserTypes 
+    {
+        public static void Run() 
+        {
+            var users = JsonConvert.DeserializeObject<List<UserWithType>>(File.ReadAllText("user_types.json")) ?? new List<UserWithType>();
+            foreach (var user in users) 
+            {
+                Console.WriteLine($"{user.name ?? "Unknown"} has the role of {user.userType ?? "unspecified"}");
+            }
         }
     }
 }
